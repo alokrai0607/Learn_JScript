4d9ad8db8369036aa30c79fd87d83deb6367a0c8
alokrai0607 <saelalok001@gmail.com>
2023-12-19 16:07:56 +0530

    COmparison Oprator

---
+++ JS_Programs/ComparisonOperators.js
@@ -0,0 +1,11 @@
+// Equality
+
+// The equality operator == lets you test if two values are equal or not. It accepts 2
+// inputs of any type and outputs true if they are equal and false if the are not
+// equal.
+
+console.log(1==1);               // true
+console.log("z"=="Z");           //false
+console.log("z"==="Z")           //false
+console.log("Masai"=="masai");   //false
+console.log("Masai"==="masai");  //false
