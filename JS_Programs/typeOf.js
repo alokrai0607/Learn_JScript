8bdcf596b35dc3094e071466979ea26c3abcd575
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:34:46 +0530

    typeOf

---
+++ JS_Programs/typeOf.js
@@ -0,0 +1,13 @@
+
+
+//String Data type
+let a = "Alok";
+console.log(typeof(a)); // String
+
+// Integer Data type
+let b = 22 ; 
+console.log(typeof(b));  // Number
+
+// double data type.
+let c = 3.14;
+console.log(typeof(c)); // Number
