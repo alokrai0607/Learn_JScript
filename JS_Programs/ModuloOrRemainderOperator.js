fda7fad6216bf6e83365529c286396f82f8595d8
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:40:51 +0530

    ModuloOr Remainder

---
+++ JS_Programs/ModuloOrRemainderOperator.js
@@ -0,0 +1,6 @@
+
+//Many programming languages including JavaScript have a modulo operator % . This
+//operator returns the remainder when one variable is divided by another.
+
+let a = 5%4;
+console.log(a);
