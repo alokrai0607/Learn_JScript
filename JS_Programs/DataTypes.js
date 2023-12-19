ffc9a7c91f5acf97a7884ceef8d2d48025183e28
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:32:26 +0530

    DataTypes

---
+++ JS_Programs/DataTypes.js
@@ -0,0 +1,14 @@
+
+//String Data type
+let a = "Alok";
+console.log(a+" is String data type");
+
+// Integer Data type
+let b = 22 ; 
+console.log(b+" is integer data type");
+
+// double data type.
+let c = 3.14;
+console.log(c+" is double datatype");
+
+// and many more ...
