a03a5c895d8a45d37af76d06e87d84f935a83f12
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:46:43 +0530

    String concatinat

--- JS_Programs/StringConcatenation.js
+++ JS_Programs/StringConcatenation.js
@@ -5,3 +5,7 @@
 var a = "Masai";
 let b = "School";
 console.log(a+" "+b);
+
+// Note: Notice the circular brackets between num1 + num2 this tells javascript that we
+// want to add the two numbers mathematically. Without the brackets the output would be
+// 1 + 2 = 12 .
