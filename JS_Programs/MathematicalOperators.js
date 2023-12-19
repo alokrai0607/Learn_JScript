89f33259420baa21251ab77023ee3a08c0695139
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:38:35 +0530

    MathmaticalOprators

---
+++ JS_Programs/MathematicalOperators.js
@@ -0,0 +1,26 @@
+// Addition
+
+let a = 5+6;
+console.log(a);
+
+
+
+
+// Substrection
+
+let b = 7-6;
+console.log(b);
+
+
+
+// Multipication
+
+let c = 9*8;
+console.log(c);
+
+
+
+// Division
+
+let d = 77/11;
+console.log(d);
