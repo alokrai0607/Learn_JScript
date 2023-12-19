bc2af8c3ddb9b911b5049e963481ecc856434edf
alokrai0607 <saelalok001@gmail.com>
2023-12-19 16:13:48 +0530

    Inequility oprator

---
+++ JS_Programs/InequalityOperator.js
@@ -0,0 +1,14 @@
+
+// The inequality operator != performs the opposite function of the equality operator.
+// It accepts 2 inputs of any type and outputs false if they are equal and true if the
+// are not equal.
+
+console.log(1!=1);   // false
+console.log(1!=2);    // true
+console.log("Masai"!="masai");  // true
+console.log("Masai"!=="masai");  // true
+console.log("Masai"!="Masai");  // false
+console.log("Masai"!=="Masai");  //false
+
+// Similar to === , !== will check for type as well.
+// It is recommended to use === and !== when it comes to comparison operators
