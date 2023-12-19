bb022c938a566067a5302fb3d56251d22920cb69
alokrai0607 <saelalok001@gmail.com>
2023-12-19 13:54:48 +0530

    Relational Oprator

---
+++ JS_Programs/RelationalOperators.js
@@ -0,0 +1,22 @@
+// These operators allow you to test the relation between 2 values and returns a
+// boolean . JavaScript unlike other languages allows you to compare any type with any
+// other type!
+
+
+// Greater than and greater than equal to------>
+
+
+// The greater than operator > allows you to check if one value is greater than the
+// other. It returns true if the first value is greater than the second and false if
+// the second value is greater.
+
+
+let a = 20 > 10;
+let b = 10 > 20
+    c = 10 > 10;
+    d = 10 >= 10;
+
+    console.log(a);  //true
+    console.log(b);  // false
+    console.log(c);   // false
+    console.log(d);   // true
