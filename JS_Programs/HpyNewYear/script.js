0f569cc4787167dc27480c4ea8a377f4d23614a0
alokrai0607 <saelalok001@gmail.com>
2023-12-31 11:58:21 +0530

    Happy new Year

---
+++ JS_Programs/HpyNewYear/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const newYearText = document.getElementById("newYearText");
+
+    // Add animation class
+    newYearText.classList.add("animate-text");
+});
